138c991ef3687caebbed558844b3f25adf8c5649
User <User@gmail.com>
2019-08-29 12:20:08 +0300

    fix gulpfile & dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,110 @@
-var syntax        = 'sass';
+"use strict";
 
-var gulp          = require('gulp'),
-		sass          = require('gulp-sass'),
-		browsersync   = require('browser-sync').create(),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require("gulp-notify"),
-		rsync         = require('gulp-rsync'),
-		pug           = require('gulp-pug'),
-		htmlmin       = require('gulp-htmlmin'),
-		imagemin      = require('gulp-imagemin'),
-		del           = require("del"),
-		wait          = require('gulp-wait');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    plumber = require("gulp-plumber"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    concat = require('gulp-concat'),
+    minify = require("gulp-csso"),
+    terser = require("gulp-terser"),
+    htmlmin = require("gulp-htmlmin"),
+    rename = require("gulp-rename"),
+    imagemin = require("gulp-imagemin"),
+    posthtml = require("gulp-posthtml"),
+    include = require("posthtml-include"),
+    pug = require("gulp-pug"),
+    del = require("del"),
+		server = require("browser-sync").create();
+		
+gulp.task("style", function() {
+  return gulp.src("source/sass/main.sass")
+    .pipe(plumber())
+    .pipe(sass({ 'include css': true }))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("main.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
 
-gulp.task('browser-sync', () => {
-	browsersync.init({
-		server: "app/",
-		notify: false,
-		open: true,
-		cors: true,
-		ui: false
-	});
+gulp.task("terser", function() {
+  return gulp.src([
+    "source/libs/**/*.js",
+    "source/js/*.js",
+    ])
+    .pipe(concat('scripts.min.js'))
+    .pipe(terser())
+    .pipe(gulp.dest("build/js"))
 });
 
 gulp.task('pug', () => {
-	return gulp.src('app/pug/pages/*.pug')
+	return gulp.src('source/pug/pages/*.pug')
 	.pipe(pug({
 		pretty: true
 	}))
-	.pipe(gulp.dest('app'))
+	.pipe(gulp.dest('build'))
 });
 
-gulp.task('styles', () => {
-	return gulp.src('app/sass/main.sass')
-	.pipe(wait(1000))
-	.pipe(sass({ 'include css': true }).on("error", notify.onError()))
-	.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-	.pipe(gulp.dest('app/css'))
-	.pipe(browsersync.stream());
+gulp.task("htmlmin", function() {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("build"));
 });
-
-gulp.task('js', () => {
-	return gulp.src([
-		'app/libs/jquery/jquery.min.js',
-		'app/libs/font-awesome/fontawesome-all.min.js',
-		'app/libs/superfish/superfish.min.js',
-		'app/libs/slick/slick.min.js',
-		'app/libs/magnific-popup/magnific-popup.min.js',
-		'app/libs/mmenu/jquery.mmenu.all.js',
-		'app/libs/mmenu/navbars/jquery.mmenu.navbars.js',
-		'app/js/common.js', // Always at the end
-		])
-	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Minify js (opt.)
-	.pipe(gulp.dest('app/js'))
-	.pipe(browsersync.stream());
-});
-
-gulp.task('transfer', () => {
-    return gulp.src([
-		"app/**",
-	])
-        .pipe(gulp.dest("dist"))
-});
-
-gulp.task("clean", () => {
-	return del(["dist/pug", "dist/sass", "dist/js/common.js"]);
-});
-
-gulp.task('html-minify', () => {
-	return gulp.src('app/*.html')
-	//   .pipe(htmlmin({collapseWhitespace: true}))
-	  .pipe(gulp.dest('dist'));
-  });
 
 gulp.task('imgmin', () =>
-  gulp.src('app/img/**/*')
+  gulp.src('source/img/**/*')
 	  .pipe(imagemin([imagemin.jpegtran({progressive: true}),
 		imagemin.svgo(), imagemin.gifsicle()]))
-	  .pipe(gulp.dest('dist/img'))
+	  .pipe(gulp.dest('build/img'))
 );
 
-gulp.task('build', gulp.series('transfer', 'clean', 'html-minify', 'imgmin'));
-
-gulp.task('rsync', () => {
-	return gulp.src('app/**')
-	.pipe(rsync({
-		root: 'app/',
-		hostname: 'username@yousite.com',
-		destination: 'yousite/public_html/',
-		// include: ['*.htaccess'], // Includes files to deploy
-		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
-		recursive: true,
-		archive: true,
-		silent: false,
-		compress: true
-	}))
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
-	gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('js'));
-	gulp.watch('app/pug/**/*.pug', gulp.parallel('pug'));
-	gulp.watch('app/*.html'), browsersync.reload;
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/**/*",
+    "source/libs/**/*",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
 });
 
-gulp.task('default', gulp.parallel('pug', 'styles', 'js', 'browser-sync', 'watch'));
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", gulp.series(
+  "clean", 
+  "copy", 
+  "style", 
+  "imgmin", 
+  "pug", 
+  "terser", 
+  "htmlmin", 
+));
+
+gulp.task("serve", function() {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.parallel("style"));
+  gulp.watch("source/*.html", gulp.parallel("html"));
+  gulp.watch('source/pug/**/*.pug', gulp.parallel('pug'));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
